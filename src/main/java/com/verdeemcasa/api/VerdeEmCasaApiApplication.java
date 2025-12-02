c4b63a6dd3b1d4f42d02c94bf9c5b7ff40e0c4c6
NatanCDalmarco <natanuy@gmail.com>
2025-12-02 17:31:09 -0300

    initial commit

---
+++ src/main/java/com/verdeemcasa/api/VerdeEmCasaApiApplication.java
@@ -0,0 +1,13 @@
+package com.verdeemcasa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdeEmCasaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerdeEmCasaApiApplication.class, args);
+    }
+
+}
